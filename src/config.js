930f9bc16b488126d252a92e9f9ef8026eea06b1
tarun922 <tarunkum8800@gmail.com>
2026-03-05 21:41:27 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // config.js — Central config. Move API_KEY to .env in production.
-export const API_KEY = 'AIzaSyA9fJlIkAduxPyWEVB5LQTMKsU2U-eBbwU';
+export const API_KEY = 'get ur key from google cloud console youtube v3 api key ';
 export const YT_BASE = 'https://www.googleapis.com/youtube/v3';
 export const DEFAULT_RESULTS = 10;
 
